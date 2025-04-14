3adb81cb18893b7bbe2520a6dd8eec187ea12863
Moaz-Darwish1 <moaz.darwish0123@gmail.com>
2025-04-14 10:27:41 +0200

    added the form and send email when submit contact us and edited profile image

---
+++ Modules/Website/app/Models/ContactUs.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Website\app\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Modules\Website\Database\factories\ContactUsFactory;
+
+class ContactUs extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'message',
+        'subject',
+    ];
+    
+    protected static function newFactory(): ContactUsFactory
+    {
+        //return ContactUsFactory::new();
+    }
+}
--- Modules/Website/resources/views/contact.blade.php
+++ Modules/Website/resources/views/contact.blade.php
@@ -3,20 +3,6 @@
 
     <main class="main">
 
-        <!-- breadcrumb -->
-        <div class="site-breadcrumb">
-            <div class="site-breadcrumb-bg" style="background: url(assets/img/breadcrumb/01.jpg)"></div>
-            <div class="container">
-                <div class="site-breadcrumb-wrap">
-                    <h4 class="breadcrumb-title">Contact Us</h4>
-                    <ul class="breadcrumb-menu">
-                        <li><a href="{{ route('home') }}"><i class="far fa-home"></i> Home</a></li>
-                        <li class="active">Contact Us</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <!-- breadcrumb end -->
 
 
         <!-- contact area -->
@@ -68,8 +54,8 @@
                                             </div>
                                             <div class="contact-info-content">
                                                 <h5>Open Time</h5>
-                                                <p>Mon - Sat (10AM - 05PM)</p>
-                                                <p>Sunday - <span class="text-danger">Closed</span></p>
+                                                <p>Sun -  Thurs(09AM - 05PM)</p>
+                                                <p>Friday -Sat <span class="text-danger">Closed</span></p>
                                             </div>
                                         </div>
                                     </div>
@@ -83,27 +69,36 @@
                                     <p>Write your problem here and we will respond to it as soon as possible </p>
                                 </div>
                                 <form method="post" action="{{route('contact.store')}}" id="contact-form">
+                                    @csrf
                                     <div class="row">
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <input type="text" class="form-control" name="name"
-                                                    placeholder="Your Name" required>
+                                            <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Your Name" >
+                                                @error('name')
+                                                    <small class="text-danger">{{ $message }}</small>
+                                                @enderror
                                             </div>
                                         </div>
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <input type="email" class="form-control" name="email"
-                                                    placeholder="Your Email" required>
+                                            <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Your Email" >
+                                                @error('email')
+                                                    <small class="text-danger">{{ $message }}</small>
+                                                @enderror
                                             </div>
                                         </div>
                                     </div>
                                     <div class="form-group">
-                                        <input type="text" class="form-control" name="subject"
-                                            placeholder="Your Subject" required>
+                                    <input type="text" class="form-control" name="subject" value="{{ old('subject') }}" placeholder="Your Subject" >
+                                        @error('subject')
+                                            <small class="text-danger">{{ $message }}</small>
+                                        @enderror
                                     </div>
                                     <div class="form-group">
-                                        <textarea name="message" cols="30" rows="4" class="form-control"
-                                            placeholder="Write Your Message" required></textarea>
+                                    <textarea name="message" cols="30" rows="4" class="form-control" placeholder="Write Your Message" >{{ old('message') }}</textarea>
+                                        @error('message')
+                                            <small class="text-danger">{{ $message }}</small>
+                                        @enderror
                                     </div>
                                     <button type="submit" class="theme-btn">Send
                                         Message <i class="far fa-paper-plane"></i></button>
@@ -125,8 +120,6 @@
         <!-- map -->
         <div class="contact-map" style="margin-bottom: 2rem;">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55657.62861611223!2d30.783552824634057!3d29.323354465146668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1459790063055567%3A0xbc3f927149ad20e8!2z2YXYudmH2K8g2KrZg9mG2YjZhNmI2KzZitinINin2YTZhdi52YTZiNmF2KfYqiBJVEkgLUZheW91bSBCcmFuY2g!5e0!3m2!1sen!2seg!4v1744541556240!5m2!1sen!2seg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96708.34194156103!2d-74.03927096447748!3d40.759040329405195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4a01c8df6fb3cb8!2sSolomon%20R.%20Guggenheim%20Museum!5e0!3m2!1sen!2sbd!4v1619410634508!5m2!1sen!2s" -->
-                <!-- style="border:0;" allowfullscreen="" loading="lazy"></iframe> -->
         </div>
         <!-- end map -->
 
