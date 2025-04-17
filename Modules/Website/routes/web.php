0d066b61457698868f8b269a4f01fb37948295ac
Moaz-Darwish1 <moaz.darwish0123@gmail.com>
2025-04-17 20:38:03 +0200

    solved the add.cart route not defined

--- Modules/Website/routes/web.php
+++ Modules/Website/routes/web.php
@@ -45,7 +45,7 @@ Route::post('/products/{product}/reviews', [ProductController::class, 'storeRevi
 // Cart Routes
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
-Route::post('/cart/add/{product}', [CartController::class, 'add_ajax'])->name('cart.add_ajax');
+Route::post('/cart/add/aj/{product}', [CartController::class, 'add_ajax'])->name('cart.add_ajax');
 Route::post('/cart/update/{productId}', [CartController::class, 'update'])->name('cart.update');
 Route::post('/cart/remove/{productId}', [CartController::class, 'remove'])->name('cart.remove');
 //Wishlist Routes
